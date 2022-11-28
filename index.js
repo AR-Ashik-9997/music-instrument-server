ac2d2a4db941f635f3a9d8ff18f290c751492a6c
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2022-11-28 20:19:53 +0600

    jwt token created

--- index.js
+++ index.js
@@ -10,4 +10,26 @@ const stripe = require("stripe")(process.env.STRIPE_KEY);
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.DB_Url;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+function varifySecret(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.Secrete_Token, function (err, decoded) {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 
